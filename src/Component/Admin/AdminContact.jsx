d39a75c7874ec63e60249a612937aa0fd0ec4e8f
akash8347 <akashgohil.av@gmail.com>
2023-12-10 23:11:01 +0530

    corrected one url that was on localhost

--- src/Component/Admin/AdminContact.jsx
+++ src/Component/Admin/AdminContact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminHed from './AdminHed'
 import { useContext } from 'react'
 import { AuthContext } from '../../context/AuthProvider'
@@ -6,80 +6,82 @@ import moment from 'moment'
 const AdminContact = () => {
   const { admin } = useContext(AuthContext)
   const [contacts, setContacts] = useState([]);
-  const [error,setError]=useState(null)
-    useEffect(() => {
+  const [error, setError] = useState(null)
+  useEffect(() => {
 
-        const func = async () => {
-          const {token}=admin
-          let url= process.env.REACT_APP_BACKENDURL
-          
-          const res = await fetch(`${url}/admin/allcontact`, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}`
-            }
-          })
-          let contactjson = await res.json()
-          if (!res.ok) {
-            console.log(contactjson.error)
-            setError(contactjson.error)
-          }
-        //   dispatch({ type: 'ALLORDERS', payload: orderjson.orders1 })
-        setContacts(contactjson.contacts);
-        console.log(contactjson.contacts)
+    const func = async () => {
+      const { token } = admin
+      let url = process.env.REACT_APP_BACKENDURL
+
+      const res = await fetch(`${url}/admin/allcontact`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
         }
-        if (admin) {
-          func()
-        }
-    
-      }, [ admin])
+      })
+      let contactjson = await res.json()
+      if (!res.ok) {
+        console.log(contactjson.error)
+        setError(contactjson.error)
+      }
+      //   dispatch({ type: 'ALLORDERS', payload: orderjson.orders1 })
+      setContacts(contactjson.contacts);
+      console.log(contactjson.contacts)
+    }
+    if (admin) {
+      func()
+    }
+
+  }, [admin])
   return (
     <>
       <h2 className='admin-h1' >contact</h2>
-     <AdminHed/>
-     {console.log(contacts) }
-    {
-      error===null?(
-        <div className="admin-container" style={{ margin: "0 auto", width: "80%", paddingBottom: "50px" }}>
-        <div className="adminorder-table">
-   
-        <table>
-           <thead>
-             <tr>
-               <th>Name</th>
-               <th>Email</th>
-               <th>Message</th>
-               <th>Created At</th>
-             </tr>
-           </thead>
-           <tbody>
-             {
-             contacts.map((contact) => {
-              const formattedDate = moment(contact.createdAt).format('D-M-YYYY');
-                  const createdAt = moment(contact.createdAt).fromNow();
-               <tr key={contact.id}>
-                 <td>{contact.name}</td>
-                 <td>{contact.email}</td>
-                 <td>{contact.message}</td>
-                 <td>{createdAt + ' ' + formattedDate}</td>
-               </tr>
-               })
-             }
-           </tbody>
-         </table>
-   
-        </div>
-   
-        </div>
-      ):(
-<>
-<div className="error">{error}</div>
-</>
-      )
-    }
+      <AdminHed />
+      {console.log(contacts)}
+      {
+        error === null ? (
+          <div className="admin-container" style={{ margin: "0 auto", width: "80%", paddingBottom: "50px" }}>
+            <div className="adminorder-table">
+
+              <table>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Message</th>
+                    <th>Created At</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {contacts.map((contact) => {
+                    const formattedDate = moment(contact.createdAt).format('D-M-YYYY');
+                    const createdAt = moment(contact.createdAt).fromNow();
+
+                    return (
+                      <tr key={contact.id}>
+                        <td>{contact.name}</td>
+                        <td>{contact.email}</td>
+                        <td>{contact.message}</td>
+                        <td>{createdAt + ' ' + formattedDate}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+
+              </table>
+
+            </div>
+
+          </div>
+        ) : (
+          <>
+            <div className="error">{error}</div>
+          </>
+        )
+      }
     </>
-  
+
   )
 }
 
