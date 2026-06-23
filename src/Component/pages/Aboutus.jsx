97101f79537cc98273adef9f16acdabf9ccb5119
akash8347 <akashgohil.av@gmail.com>
2026-06-23 22:02:16 +0530

    updated about  us page

--- src/Component/pages/Aboutus.jsx
+++ src/Component/pages/Aboutus.jsx
@@ -9,47 +9,7 @@ const Aboutus = () => {
   <div className="about-container">
    <div className="paragraphs">
     <div className="para1 para">
-    Lorem ipsum, dolor sit amet consectetur 
-    adipisicing elit. Similique explicabo dicta 
-    ex qui sed nulla culpa aliquid dolorem,
-     numquam tenetur dolor ipsa recusandae 
-     eaque, eius quos, sequi nisi repellendus? 
-     Itaque incidunt dolorem impedit quae atque
-      molestias, recusandae sed obcaecati hic! 
-      Ipsam cum eos reiciendis. Unde est vel
-       ipsum aperiam voluptatibus error vitae 
-       fugiat alias dignissimos accusamus quam
-        culpa laborum recusandae doloremque 
-        explicabo impedit sapiente obcaecati,
-         enim tempore dicta nesciunt labore 
-         sed eos! Sit, debitis ducimus quo 
-         repellat placeat dolorem neque, sint 
-         nihil ipsa voluptas porro nemo 
-         distinctio enim ab praesentium dolorum
-          expedita at error dignissimos fugit 
-    laboriosam! Recusandae, veritatis voluptatum.
-    </div>
-    <div className="para2 para">
-
-    Lorem ipsum, dolor sit amet consectetur 
-    adipisicing elit. Similique explicabo dicta 
-    ex qui sed nulla culpa aliquid dolorem,
-     numquam tenetur dolor ipsa recusandae 
-     eaque, eius quos, sequi nisi repellendus? 
-     Itaque incidunt dolorem impedit quae atque
-      molestias, recusandae sed obcaecati hic! 
-      Ipsam cum eos reiciendis. Unde est vel
-       ipsum aperiam voluptatibus error vitae 
-       fugiat alias dignissimos accusamus quam
-        culpa laborum recusandae doloremque 
-        explicabo impedit sapiente obcaecati,
-         enim tempore dicta nesciunt labore 
-         sed eos! Sit, debitis ducimus quo 
-         repellat placeat dolorem neque, sint 
-         nihil ipsa voluptas porro nemo 
-         distinctio enim ab praesentium dolorum
-          expedita at error dignissimos fugit 
-    laboriosam! Recusandae, veritatis voluptatum.
+    A Simple E-commerce Website is a web application that allows users to browse and purchase products online. It typically includes features such as product listings, shopping carts, payment processing, and order tracking. The website may also include user accounts, product reviews, and customer support options. A Simple E-commerce Website can be built using various technologies such as HTML, CSS, JavaScript, and server-side programming languages like PHP or Python. It can be hosted on a web server and accessed through a web browser on desktop or mobile devices.
     </div>
    </div>
   </div>
